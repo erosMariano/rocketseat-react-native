0333bffcba9f0c34f203f4eb383662a6ad850ba3
erosMariano <erosmariano1@gmail.com>
2023-06-20 21:25:44 -0300

    feat: create, read groups and players

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Container } from "./styles";
 import Header from "@components/Header";
 import HighLight from "@components/Highlight";
@@ -6,15 +6,35 @@ import GroupCard from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 export default function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
   const navigation = useNavigation();
 
   function handleNewGroup() {
-    navigation.navigate("new")
+    navigation.navigate("new");
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+  useFocusEffect(
+    useCallback(() => {
+      console.log("UseFocus effect");
+      fetchGroups();
+    }, [])
+  );
   return (
     <Container>
       <Header />
@@ -23,7 +43,9 @@ export default function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -5,14 +5,33 @@ import HighLight from "@components/Highlight";
 import { Button } from "@components/Button";
 import Input from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export default function NewGroup() {
   const [group, setGroup] = useState("");
 
   const navigation = useNavigation();
-  function handleNew() {
-    navigation.navigate("players", { group });
+
+  async function handleNew() {
+    if(group.trim().length === 0){
+      return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+    }
+
+    try {
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Não foi possível criar novo grupo.");
+        console.log(error);
+      }
+    }
   }
+
   return (
     <Container>
       <Header showBackButton />
